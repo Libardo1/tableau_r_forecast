24c3cbe8617acf618b6663bc9a3d149fd6b12be1
Keberwein <kristian.eberwein@yahoo.com>
2017-02-03 09:17:42 -0500

    added Arima

--- Superstore_Forecast.R
+++ Superstore_Forecast.R
@@ -3,19 +3,16 @@ library(forecast)
 SuperstoreSales$Order.Date <- as.Date(as.character(SuperstoreSales$Order.Date), format="%m/%d/%y")
 # Create time sereis
 time <- ts(data=SuperstoreSales$Sales, start=c(2010, 1), deltat=1/12)
-
-# Arima Model
-#time <- Arima(window(time),order=c(0,1,1),
-#                   seasonal=list(order=c(0,1,1),period=12),lambda=0)# Fit into forecsast model
-#fcast <- forecast.Arima(time)
-
+# Forecast
 fcast <- forecast(time)
+
+# Or, you can do an Arima Model
+time <- ts(data=SuperstoreSales$Sales, start=c(2010, 1), deltat=1/12)
+timeArima <- auto.arima(time)
+fcastArima <- forecast.Arima(timeArima)
+
 # Determine accuracy of fit
 accuracy(fcast)
 # Plot fitted model
 plot(forecast(time))
 lines(fitted(fcast),col="blue")
-
-SuperstoreSales <- read.csv("SuperstoreSales.csv")
-
-z=WWWusage
